a1a970ef26a3b50bbcba3d7cf6d68ca2d3b845a2
Ming Ying <ming.ying.nyc@gmail.com>
2024-08-27 20:35:54 -0400

    forward/back buttons

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,6 +1,9 @@
 "use client"
 
+import { Button } from "@/components/Button"
+import { ArrowAnimated } from "@/components/ui/ArrowAnimated"
 import classNames from "classnames"
+import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { siteConfig } from "../siteConfig"
 
@@ -36,12 +39,50 @@ const navigation = [
   },
 ]
 
+const BackButton = ({ href }: { href: string }) => (
+  <Link href={href}>
+    <Button
+      className="group bg-transparent px-0 text-gray-600 hover:bg-transparent"
+      variant="light"
+    >
+      <ArrowAnimated
+        className="relative right-3 rotate-180 transform stroke-gray-600"
+        aria-hidden="true"
+      />
+      <div>Previous Post</div>
+    </Button>
+  </Link>
+)
+
+const NextButton = ({ href }: { href: string }) => (
+  <Link href={href}>
+    <Button
+      className="group bg-transparent px-0 text-gray-600 hover:bg-transparent"
+      variant="light"
+    >
+      Next Post
+      <ArrowAnimated className="stroke-gray-600" aria-hidden="true" />
+    </Button>
+  </Link>
+)
+
 export default function BlogLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode
 }) {
   const pathname = usePathname()
+  const currentPageIdx = navigation.findIndex((item) =>
+    pathname.endsWith(item.href),
+  )
+  const canGoBackward = currentPageIdx > 0
+  const canGoForward = currentPageIdx < navigation.length - 1
+  const nextHref = canGoForward
+    ? `${siteConfig.baseLinks.blog}/${navigation[currentPageIdx + 1].href}`
+    : ""
+  const previousHref = canGoBackward
+    ? `${siteConfig.baseLinks.blog}/${navigation[currentPageIdx - 1].href}`
+    : ""
 
   return (
     <>
@@ -76,8 +117,16 @@ export default function BlogLayout({
             </div>
           </div>
 
-          <main>
-            <div className="px-4 py-12 sm:px-6 sm:py-0 lg:px-8">{children}</div>
+          <main className="w-full max-w-2xl px-4 py-4 md:py-0 lg:px-8">
+            <div className="mx-auto flex justify-between">
+              {canGoBackward && <BackButton href={previousHref} />}
+              {canGoForward && <NextButton href={nextHref} />}
+            </div>
+            <div className="py-4">{children}</div>
+            <div className="mx-auto flex justify-between">
+              {canGoBackward && <BackButton href={previousHref} />}
+              {canGoForward && <NextButton href={nextHref} />}
+            </div>
           </main>
         </div>
       </div>
