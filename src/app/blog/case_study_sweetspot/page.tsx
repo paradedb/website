476bc9470570e0741e8330e037f9db6b90b25554
Ming Ying <ming.ying.nyc@gmail.com>
2024-08-28 15:25:54 -0400

    fix sweetspot case study

--- src/app/blog/case_study_sweetspot/page.tsx
+++ src/app/blog/case_study_sweetspot/page.tsx
@@ -4,7 +4,7 @@ import Content from "../../../components/markdown/case_study_sweetspot.mdx"
 
 export default function Blog() {
   return (
-    <div className="prose w-full max-w-xl">
+    <div className="prose w-full max-w-4xl">
       <Content />
     </div>
   )
