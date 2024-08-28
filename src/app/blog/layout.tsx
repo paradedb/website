3d5ec4b61406f42fc217b083acf3ec273d7b2c8c
Ming Ying <ming.ying.nyc@gmail.com>
2024-08-28 09:39:36 -0400

    fix blog padding

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -52,51 +52,48 @@ export default function BlogLayout({
     : ""
 
   return (
-    <>
-      <div>
-        <div className="mx-auto flex max-w-6xl md:mt-12">
-          <div className="hidden lg:flex lg:w-96 lg:flex-col">
-            {/* Sidebar component, swap this element with another sidebar if you like */}
-            <div className="flex grow flex-col gap-y-5 overflow-y-auto px-6 pb-4">
-              <nav className="flex flex-1 flex-col">
-                <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                  <li>
-                    <ul role="list" className="-mx-2 space-y-1">
-                      {blog.map((item) => (
-                        <li key={item.name}>
-                          <a
-                            href={`${siteConfig.baseLinks.blog}/${item.href}`}
-                            className={classNames(
-                              pathname.endsWith(item.href)
-                                ? "bg-gray-50 text-indigo-600"
-                                : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
-                              "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
-                            )}
-                          >
-                            {item.name}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  </li>
+    <div className="mx-auto flex max-w-6xl md:mt-12">
+      <div className="hidden lg:flex lg:w-96 lg:flex-col">
+        {/* Sidebar component */}
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto px-6 pb-4">
+          <nav className="flex flex-1 flex-col">
+            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+              <li>
+                <ul role="list" className="-mx-2 space-y-1">
+                  {blog.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={`${siteConfig.baseLinks.blog}/${item.href}`}
+                        className={classNames(
+                          pathname.endsWith(item.href)
+                            ? "bg-gray-50 text-indigo-600"
+                            : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
+                          "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
+                        )}
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
                 </ul>
-              </nav>
-            </div>
-          </div>
-
-          <main className="w-full px-4 py-4 md:py-0 lg:px-8">
-            <div className="mx-auto flex justify-between">
-              {canGoBackward && <BackButton href={previousHref} />}
-              {canGoForward && <NextButton href={nextHref} />}
-            </div>
-            <div className="w-full py-4">{children}</div>
-            <div className="mx-auto flex justify-between">
-              {canGoBackward && <BackButton href={previousHref} />}
-              {canGoForward && <NextButton href={nextHref} />}
-            </div>
-          </main>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
-    </>
+
+      {/* Blog content */}
+      <main className="w-full px-4 px-8 py-4 md:py-0">
+        <div className="mx-auto flex justify-between">
+          {canGoBackward && <BackButton href={previousHref} />}
+          {canGoForward && <NextButton href={nextHref} />}
+        </div>
+        <div className="w-full py-4">{children}</div>
+        <div className="mx-auto flex justify-between">
+          {canGoBackward && <BackButton href={previousHref} />}
+          {canGoForward && <NextButton href={nextHref} />}
+        </div>
+      </main>
+    </div>
   )
 }
