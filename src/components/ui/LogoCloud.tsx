058ad735c31ffa8582c1c750de642ca678d7253c
Ming Ying <ming.ying.nyc@gmail.com>
2024-12-19 17:57:53 -0500

    add logos

--- src/components/ui/LogoCloud.tsx
+++ src/components/ui/LogoCloud.tsx
@@ -1,20 +1,39 @@
 import { Logos } from "./Logos"
+import classNames from "classnames"
+
+const logoCellStyle = "px-4 sm:px-12 flex justify-center h-16 sm:h-20"
+const bottomDotted = "border-dotted border-b border-slate-200"
+const rightDotted = "border-dotted border-r border-slate-200"
 
 export default function LogoCloud() {
   return (
     <section
       aria-label="Company logos"
-      className="mt-24 flex animate-slide-up-fade flex-col items-center justify-center gap-y-6 text-center"
+      className="mx-auto mt-24 flex max-w-6xl animate-slide-up-fade flex-col items-center justify-center gap-y-6 text-center"
       style={{ animationDuration: "1500ms" }}
     >
       <div className="text-md mx-auto font-medium text-gray-900">
         Trusted by enterprises, loved by developers
       </div>
-      <div className="-mt-2 flex space-x-4 sm:space-x-10 sm:space-x-6 md:mt-0">
-        <Logos.Alibaba className="w-20 sm:w-28" />
-        <Logos.DemandScience className="w-24 sm:w-36" />
-        <Logos.Zulip className="w-12 sm:w-16" />
-        <Logos.Insa className="w-12 sm:w-16" />
+      <div className="grid grid-cols-3">
+        <div className={classNames(logoCellStyle, bottomDotted, rightDotted)}>
+          <Logos.BiltRewards className="mx-auto w-12 sm:w-16" />
+        </div>
+        <div className={classNames(logoCellStyle, bottomDotted, rightDotted)}>
+          <Logos.ModernTreasury className="mx-auto w-36 sm:w-40" />
+        </div>
+        <div className={classNames(logoCellStyle, bottomDotted)}>
+          <Logos.Alibaba className="mx-auto w-20 sm:w-24" />
+        </div>
+        <div className={classNames(logoCellStyle, rightDotted)}>
+          <Logos.DemandScience className="mx-auto w-24 sm:w-36" />
+        </div>
+        <div className={classNames(logoCellStyle, rightDotted)}>
+          <Logos.Zulip className="mx-auto w-12 sm:w-16" />
+        </div>
+        <div className={logoCellStyle}>
+          <Logos.Insa className="mx-auto w-12 sm:w-16 my-auto" />
+        </div>
       </div>
     </section>
   )
