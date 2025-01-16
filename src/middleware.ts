d3f256239d996f8db8b3449d678bdcc7e7048625
Ming <ming.ying.nyc@gmail.com>
2025-01-16 09:11:47 -0800

    block storage post part 1 (#24)
    Signed-off-by: Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
    Co-authored-by: Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
    Co-authored-by: Philippe Noël <philippemnoel@gmail.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export async function middleware(request: NextRequest) {
 
   // If the user agent is curl, return the deploy script
   if (userAgent.startsWith("curl/")) {
-    const response = await fetch("https://raw.githubusercontent.com/paradedb/paradedb/refs/heads/main/deploy.sh")
+    const response = await fetch("https://raw.githubusercontent.com/paradedb/paradedb/refs/heads/main/install.sh")
     const scriptContent = await response.text()
     return new NextResponse(scriptContent, {
       status: 200,
