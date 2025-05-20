06b0bed161d89fd14818604a15c70b81e2db8f38
Ming Ying <ming.ying.nyc@gmail.com>
2025-05-20 15:43:59 -0400

    improve mobile

--- src/components/ui/LogoCloud.tsx
+++ src/components/ui/LogoCloud.tsx
@@ -15,30 +15,33 @@ export default function LogoCloud() {
       <div className="text-md mx-auto font-medium text-gray-900">
         Trusted by enterprises, loved by developers
       </div>
-      <div className="flex flex-wrap justify-center gap-x-6 sm:gap-x-12 mt-6 gap-y-2">
+      <div className="flex flex-wrap justify-center gap-x-6 sm:gap-x-12 mt-6 gap-y-4">
         <div className={classNames(logoCellStyle, bottomDotted, rightDotted)}>
-          <Logos.BiltRewards className="mx-auto w-12 sm:w-16" />
+          <Logos.BiltRewards className="mx-auto w-14 sm:w-16" />
         </div>
         <div className={classNames(logoCellStyle, bottomDotted, rightDotted)}>
-          <Logos.ModernTreasury className="mx-auto w-32 sm:w-40" />
+          <Logos.ModernTreasury className="mx-auto w-36 sm:w-40" />
         </div>
         <div className={classNames(logoCellStyle, bottomDotted)}>
           <Logos.Alibaba className="mx-auto w-20 sm:w-24" />
         </div>
         <div className={classNames(logoCellStyle, bottomDotted, rightDotted)}>
-          <Logos.Tcdi className="mx-auto w-8 sm:w-10 pb-1" />
+          <Logos.Tcdi className="mx-auto w-8 sm:w-10 sm:pb-1" />
         </div>
         <div className={classNames(logoCellStyle, rightDotted)}>
-          <Logos.DemandScience className="mx-auto w-24 sm:w-36" />
+          <Logos.DemandScience className="mx-auto w-32 sm:w-36" />
         </div>
         <div className={classNames(logoCellStyle, bottomDotted)}>
           <Logos.RxVantage className="mx-auto w-24 sm:w-28 pt-0.5" />
         </div>
         <div className={logoCellStyle}>
-          <Logos.Cofactr className="mx-auto w-16 sm:w-20 my-auto" />
+          <Logos.Cofactr className="mx-auto w-20 sm:w-20 my-auto" />
         </div>
         <div className={classNames(logoCellStyle, rightDotted)}>
-          <Logos.Zulip className="mx-auto w-12 sm:w-16" />
+          <Logos.Zulip className="mx-auto w-16 sm:w-16" />
+        </div>
+        <div className={classNames(logoCellStyle, rightDotted, "sm:hidden")}>
+          <Logos.Insa className="mx-auto w-16 sm:w-16" />
         </div>
       </div>
     </section>
