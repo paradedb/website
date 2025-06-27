de9d4b038239211500ebee0babea9bde80541381
Ming Ying <ming.ying.nyc@gmail.com>
2025-06-27 13:19:32 -0400

    prettier

--- src/app/blog/lsm_trees_in_postgres/layout.tsx
+++ src/app/blog/lsm_trees_in_postgres/layout.tsx
@@ -4,10 +4,10 @@ import { blog } from "@/lib/links";
 const post = blog.find((post) => post.href === "lsm_trees_in_postgres");
 
 export const metadata: Metadata = {
-    title: post?.name,
-    description: post?.description,
+  title: post?.name,
+  description: post?.description,
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return children;
+  return children;
 }
