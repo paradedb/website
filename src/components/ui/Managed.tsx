42cbd2dc494f763f7d3842d77d6af585f002a2fb
Ming Ying <ming.ying.nyc@gmail.com>
2025-07-29 09:31:53 -0400

    prettier

--- src/components/ui/Managed.tsx
+++ src/components/ui/Managed.tsx
@@ -12,32 +12,35 @@ import { useState } from "react";
 
 const caseStudies = [
   {
-    "metric": 95,
-    "unit": "%",
-    "description": "Fewer Query Timeouts",
-    "logo": () => <Bilt fill="white" className="h-6 md:h-8" />,
-    "link": blog.find(b => b.href === "case_study_bilt")?.href,
-    "bgStyle": "bg-gray-900",
-    "textStyle": "text-slate-100",
-    "hoverColor": "rgba(255,255,255,0.2)"
+    metric: 95,
+    unit: "%",
+    description: "Fewer Query Timeouts",
+    logo: () => <Bilt fill="white" className="h-6 md:h-8" />,
+    link: blog.find((b) => b.href === "case_study_bilt")?.href,
+    bgStyle: "bg-gray-900",
+    textStyle: "text-slate-100",
+    hoverColor: "rgba(255,255,255,0.2)",
   },
   {
-    "metric": 500,
-    "unit": "%",
-    "description": "Higher Search Throughput",
-    "logo": () => <Alibaba fill="black" className="h-6 md:h-8" />,
-    "link": blog.find(b => b.href === "case_study_alibaba")?.href,
-    "bgStyle": "bg-orange-50",
-    "textStyle": "text-gray-900",
-    "hoverColor": "rgba(255,102,0,0.5)"
-  }
-]
+    metric: 500,
+    unit: "%",
+    description: "Higher Search Throughput",
+    logo: () => <Alibaba fill="black" className="h-6 md:h-8" />,
+    link: blog.find((b) => b.href === "case_study_alibaba")?.href,
+    bgStyle: "bg-orange-50",
+    textStyle: "text-gray-900",
+    hoverColor: "rgba(255,102,0,0.5)",
+  },
+];
 
 export default function SearchAnalytics() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const [isHovering, setIsHovering] = useState<number | null>(null);
 
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>, index: number) => {
+  const handleMouseMove = (
+    e: React.MouseEvent<HTMLDivElement>,
+    index: number,
+  ) => {
     const rect = e.currentTarget.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
@@ -59,18 +62,23 @@ export default function SearchAnalytics() {
         Battled-Tested in Production
       </h2>
       <p className="mt-2 max-w-2xl text-gray-600 md:mt-6 md:text-lg">
-        ParadeDB powers search and analytics for some of the most demanding enterprise applications.
+        ParadeDB powers search and analytics for some of the most demanding
+        enterprise applications.
       </p>
       <div className="mt-12 grid grid-cols-2 gap-x-8 gap-y-4">
         {caseStudies.map((study, index) => (
           <div
             key={index}
-            className="relative col-span-2 h-max max-w-2xl animate-slide-up-fade rounded-2xl sm:ml-auto sm:w-full md:col-span-1 group"
+            className="relative col-span-2 h-max max-w-2xl animate-slide-up-fade sm:ml-auto sm:w-full md:col-span-1 group"
             style={{ animationDuration: "1400ms" }}
           >
             <Link href={`/blog/${study.link}`} target="_blank">
               <div
-                className={classNames("rounded-xl relative overflow-hidden", study.bgStyle, "py-8 px-8 hover:cursor-pointer duration-300")}
+                className={classNames(
+                  "rounded-lg relative overflow-hidden",
+                  study.bgStyle,
+                  "py-8 px-8 hover:cursor-pointer duration-300",
+                )}
                 onMouseMove={(e) => handleMouseMove(e, index)}
                 onMouseLeave={handleMouseLeave}
               >
@@ -78,14 +86,14 @@ export default function SearchAnalytics() {
                 <div
                   className="absolute inset-0 transition-opacity duration-300"
                   style={{
-                    background: index === 1
-                      ? `radial-gradient(1000px circle at calc(0% - 16px) calc(100% + 16px), ${study.hoverColor}, transparent 60%)`
-                      : (isHovering === index
-                        ? `radial-gradient(1000px circle at ${mousePosition.x}px ${mousePosition.y}px, ${study.hoverColor}, transparent 60%)`
-                        : `radial-gradient(1000px circle at calc(100% + 16px) calc(100% + 16px), ${study.hoverColor}, transparent 60%)`
-                      ),
-                    pointerEvents: 'none',
-                    zIndex: 1
+                    background:
+                      index === 1
+                        ? `radial-gradient(1000px circle at calc(0% - 16px) calc(100% + 16px), ${study.hoverColor}, transparent 60%)`
+                        : isHovering === index
+                          ? `radial-gradient(1000px circle at ${mousePosition.x}px ${mousePosition.y}px, ${study.hoverColor}, transparent 60%)`
+                          : `radial-gradient(1000px circle at calc(100% + 16px) calc(100% + 16px), ${study.hoverColor}, transparent 60%)`,
+                    pointerEvents: "none",
+                    zIndex: 1,
                   }}
                 />
 
@@ -95,22 +103,34 @@ export default function SearchAnalytics() {
                     className="absolute inset-0 transition-opacity duration-300"
                     style={{
                       background: `radial-gradient(1000px circle at ${mousePosition.x}px ${mousePosition.y}px, ${study.hoverColor}, transparent 50%)`,
-                      pointerEvents: 'none',
-                      zIndex: 2
+                      pointerEvents: "none",
+                      zIndex: 2,
                     }}
                   />
                 )}
 
                 <div className="relative z-10">
                   <study.logo />
-                  <div className={classNames("text-6xl md:text-8xl font-medium", study.textStyle, "mt-12 md:mt-20")}>
-                    {study.metric}<span className="text-5xl md:text-7xl font-normal">{study.unit}</span>
+                  <div
+                    className={classNames(
+                      "text-6xl md:text-8xl font-medium",
+                      study.textStyle,
+                      "mt-12 md:mt-20",
+                    )}
+                  >
+                    {study.metric}
+                    <span className="text-5xl md:text-7xl font-normal">
+                      {study.unit}
+                    </span>
                   </div>
                   <p className={classNames("mt-2", study.textStyle)}>
                     {study.description}
                   </p>
                   <Button
-                    className={classNames("group mt-12 md:mt-16 bg-transparent hover:bg-transparent px-0", study.textStyle)}
+                    className={classNames(
+                      "group mt-12 md:mt-16 bg-transparent hover:bg-transparent px-0",
+                      study.textStyle,
+                    )}
                     variant="light"
                   >
                     Read Story
@@ -125,6 +145,6 @@ export default function SearchAnalytics() {
           </div>
         ))}
       </div>
-    </section >
+    </section>
   );
 }
