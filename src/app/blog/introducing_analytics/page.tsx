32b887ba09ad62f865daa68aa10fbad36095c967
Philippe Noël <philippemnoel@gmail.com>
2025-08-02 11:21:48 -0400

    Remove some links left behind

--- src/app/blog/introducing_analytics/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-import { siteConfig } from "../../siteConfig";
-
-export default function Blog() {
-  redirect(`${siteConfig.baseLinks.blog}/introducing_lakehouse`);
-}
