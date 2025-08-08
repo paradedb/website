3d146927ea34839f06a3f96fffa7728a96f03e35
Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
2025-08-08 12:07:01 -0700

    fix: Fix blog redirect, and redirect outdated pg_analytics and pg_lakehouse posts over to pg_search (#59)

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,32 @@ const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'blog.paradedb.com',
+          },
+        ],
+        destination: 'https://paradedb.com/blog/:path*',
+        permanent: true,
+      },
+      {
+        source: '/blog/introducing_lakehouse',
+        destination: 'https://www.paradedb.com/blog/introducing_search',
+        permanent: true,
+      },
+      {
+        source: '/blog/introducing_analytics',
+        destination: 'https://www.paradedb.com/blog/introducing_search',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 const withMDX = createMDX({
