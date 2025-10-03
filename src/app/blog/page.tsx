c6e6fbb990633d1c212e1ea2dfe0994c5d7527aa
James Blackwood-Sewell <james.sewell@gmail.com>
2025-10-03 19:57:42 +1300

    Remove the buidlscript system

--- src/app/blog/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getBlogLinks } from "@/lib/blog";
-import { redirect } from "next/navigation";
-import { siteConfig } from "../siteConfig";
-
-export default async function Blog() {
-  const blogLinks = await getBlogLinks();
-  redirect(`${siteConfig.baseLinks.blog}/${blogLinks[0]?.href || ""}`);
-}
