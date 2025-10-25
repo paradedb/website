fdda319fb325fecf526ee351c84732779fd18115
Philippe Noël <philippemnoel@gmail.com>
2025-10-25 19:08:28 +0300

    Add Learn to footer so there's more than a single link

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -14,6 +14,7 @@ import { DatabaseLogo } from "../../../public/DatabaseLogo";
 const navigation = {
   company: [
     { name: "Blog", href: siteConfig.baseLinks.blog, external: false },
+    { name: "Learn", href: siteConfig.baseLinks.resources, external: false },
     { name: "Documentation", href: documentation.BASE, external: true },
     { name: "Changelog", href: documentation.CHANGELOG, external: true },
   ],
