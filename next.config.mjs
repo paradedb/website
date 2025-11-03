37688c3843dfc99c16dac4605f6bc22036651ba7
James Blackwood-Sewell <james.sewell@gmail.com>
2025-11-03 21:40:12 +1300

    Add redirects for _ -> -
    Fix changes made in 9d258f49e021b4b688c526e41c20cf2500ad1bea to redirect old links with
    underscores to the new ones with hyphens

--- next.config.mjs
+++ next.config.mjs
@@ -38,6 +38,71 @@ const nextConfig = {
         destination: "https://www.paradedb.com/blog/introducing-search",
         permanent: true,
       },
+      {
+        source: "/blog/block_storage_part_one",
+        destination: "https://www.paradedb.com/blog/block-storage-part-one",
+        permanent: true,
+      },
+      {
+        source: "/blog/case_study_alibaba",
+        destination: "https://www.paradedb.com/blog/case-study-alibaba",
+        permanent: true,
+      },
+      {
+        source: "/blog/case_study_bilt",
+        destination: "https://www.paradedb.com/blog/case-study-bilt",
+        permanent: true,
+      },
+      {
+        source: "/blog/case_study_insa",
+        destination: "https://www.paradedb.com/blog/case-study-insa",
+        permanent: true,
+      },
+      {
+        source: "/blog/case_study_sweetspot",
+        destination: "https://www.paradedb.com/blog/case-study-sweetspot",
+        permanent: true,
+      },
+      {
+        source: "/blog/elasticsearch_vs_postgres",
+        destination: "https://www.paradedb.com/blog/elasticsearch-vs-postgres",
+        permanent: true,
+      },
+      {
+        source: "/blog/etl_vs_logical_replication",
+        destination: "https://www.paradedb.com/blog/etl-vs-logical-replication",
+        permanent: true,
+      },
+      {
+        source: "/blog/improving_documentation",
+        destination: "https://www.paradedb.com/blog/improving-documentation",
+        permanent: true,
+      },
+      {
+        source: "/blog/introducing_paradedb",
+        destination: "https://www.paradedb.com/blog/introducing-paradedb",
+        permanent: true,
+      },
+      {
+        source: "/blog/introducing_search",
+        destination: "https://www.paradedb.com/blog/introducing-search",
+        permanent: true,
+      },
+      {
+        source: "/blog/introducing_sparse",
+        destination: "https://www.paradedb.com/blog/introducing-sparse",
+        permanent: true,
+      },
+      {
+        source: "/blog/lsm_trees_in_postgres",
+        destination: "https://www.paradedb.com/blog/lsm-trees-in-postgres",
+        permanent: true,
+      },
+      {
+        source: "/blog/series_a_announcement",
+        destination: "https://www.paradedb.com/blog/series-a-announcement",
+        permanent: true,
+      },
     ];
   },
 };
