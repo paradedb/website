ba908c3e5648026b120ffe631927ccad7f070b18
James Blackwood-Sewell <james.sewell@gmail.com>
2026-02-14 08:56:55 +1300

    Add button

---
+++ src/components/CopyButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+
+export function CopyButton({ code }: { code: string }) {
+  const [copied, setCopied] = React.useState(false);
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(code);
+      setCopied(true);
+    } catch (error) {
+      console.error("Error copying to clipboard", error);
+    } finally {
+      setTimeout(() => {
+        setCopied(false);
+      }, 1500);
+    }
+  };
+
+  return (
+    <button
+      onClick={copyToClipboard}
+      className="text-sm text-white font-mono font-medium hover:text-white/80 transition-colors"
+    >
+      [{copied ? "copied" : "copy"}]
+    </button>
+  );
+}
